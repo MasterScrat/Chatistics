8f4e523bb821dd4aca7ed4a47997c29d731985ea
flaurent <florian.laurent@gmail.com>
2016-10-23 17:13:53 +0200

    Added args support and cleaned up histograms part

--- analyse.py
+++ analyse.py
@@ -6,38 +6,60 @@ import numpy as np
 from ggplot import *
 from matplotlib import pyplot as plt
 import sys
-
-filterConversation = None
-plotDensity = False
-
-TOP_N = 20;
+import argparse
 
 # avoids unicode errors
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-df1 = pd.read_pickle('data/hangouts.pkl')
-df2 = pd.read_pickle('data/messenger.pkl')
-df = pd.concat([df1])
+# arguments parsing
+parser = argparse.ArgumentParser()
+parser.add_argument('-d','--data', dest='dataPaths', nargs='+', help='chat log data files (pickle files)', required=True)
+parser.add_argument("-plotDensity", dest='density', action='store_true', help="plots the message densities (KDE) instead of their count")
+parser.add_argument("-n", "-numberSenders", dest='topN', type=int, default=10, help="number of different senders to consider, ordered by number of messages sent")
+parser.add_argument("-b", "-binWidth", dest='binWidth', type=int, default=25, help="bin width for histograms")
+parser.add_argument("-filterConversation", dest='filterConversation', type=str, default=None, help="only keep messages sent in a conversation with this sender")
+parser.add_argument("-filterSender", dest='filterSender', type=str, default=None, help="only keep messages sent by this sender")
+parser.add_argument("-removeSender", dest='removeSender', type=str, default=None, help="remove messages sent by this sender")
+args = parser.parse_args()
 
-df.columns = ['timestamp', 'conversationWithId', 'conversationWithName','senderId', 'senderName', 'text']
+dataPaths = args.dataPaths
+topN = args.topN;
+binWidth = args.binWidth
+filterConversation = args.filterConversation
+filterSender = args.filterSender
+plotDensity = args.density
+removeSender = args.removeSender
 
-# keeps only TOP_N interlocutors
+# data loading
+df = pd.DataFrame()
+for dataPath in dataPaths:
+    print 'Loading', dataPath
+    df = pd.concat([df, pd.read_pickle(dataPath)])
+
+df.columns = ['timestamp', 'conversationWithName', 'senderName', 'text']
+
+# filtering
+if filterConversation != None:
+    df = df[df['conversationWithName'] == filterConversation]
+
+if filterSender != None:
+    df = df[df['senderName'] == filterSender]
+
+if removeSender != None:
+    df = df[df['senderName'] != removeSender]
+
+# keep only topN interlocutors
 mf = df.groupby(['conversationWithName'], as_index=False) \
         .agg(lambda x:len(x))\
         .sort_values('timestamp', ascending=False)['conversationWithName']\
-        .head(TOP_N - 1)\
+        .head(topN-1)\
         .to_frame()
 
 merged = pd.merge(df, mf, on=['conversationWithName'], how='inner')
 merged = merged[['timestamp', 'conversationWithName', 'senderName']]
 
-if filterConversation != None:
-    merged = merged[merged['conversationWithName'] == filterConversation]
-
-#merged = merged[merged['senderName'] != 'Florian Laurent']
-#merged = merged[merged['senderName'] != 'florian.laurent@gmail.com']
-
+# rendering
 if plotDensity == True:
     plot = ggplot(aes(x='timestamp', color='conversationWithName', fill='conversationWithName'), data=merged) \
     + geom_density(alpha=0.6) \
@@ -48,11 +70,11 @@ if plotDensity == True:
     #+ labs(color = "Interlocutor")
 else:
     plot = ggplot(aes(x='timestamp', color='senderName', fill='senderName'), data=merged) \
-    + geom_histogram(alpha=0.6, binwidth=20) \
-    + scale_x_date(labels='%b %Y', breaks='3 months') \
+    + geom_histogram(alpha=0.6, binwidth=binWidth) \
+    + scale_x_date(labels='%b %Y', breaks='6 months') \
     + ggtitle("Message Breakdown") \
     + ylab("Number of Messages")\
     + xlab("Date")
-    #+ labs(color = "Interlocutor")
+    #+ labs(color = "Sender Name")
 
 print plot
