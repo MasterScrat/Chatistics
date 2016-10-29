4d358b7341bb8e37d1e474a5cba27537cd2b4007
flaurent <florian.laurent@gmail.com>
2016-10-29 14:19:51 +0200

    Experimented with sentiment analysis, new types of plots

--- experiments.py
+++ experiments.py
@@ -3,6 +3,8 @@ import numpy as np
 from ggplot import *
 import sys
 from random import randint
+import datetime
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -19,12 +21,12 @@ print 'Loaded', len(df), 'messages'
 # RANDOM EXPERIMENTS IN PROGRESS BELOW
 
 # computes average word length
-df['length'] = df.apply(lambda x: 0 if x.text==None else len(x.text), axis=1)
-df['nwords'] = df.apply(lambda x: 0 if x.text==None else len(x.text.split()), axis=1)
-df['awlength'] = df.apply(lambda x: 0 if x.text==None else sum(len(word) for word in x.text.split())/(x['nwords']+1.0), axis=1)
+#df3['length'] = df3.apply(lambda x: 0 if x.text==None else len(x.text), axis=1)
+#df['nwords'] = df.apply(lambda x: 0 if x.text==None else len(x.text.split()), axis=1)
+#df['awlength'] = df.apply(lambda x: 0 if x.text==None else sum(len(word) for word in x.text.split())/(x['nwords']+1.0), axis=1)
 
 # keeps only topN senders
-topN = 5
+topN = 15
 mf = df.groupby(['conversationWithName'], as_index=False) \
         .agg(lambda x:len(x))\
         .sort_values('timestamp', ascending=False)['conversationWithName']\
@@ -32,6 +34,25 @@ mf = df.groupby(['conversationWithName'], as_index=False) \
         .to_frame()
 
 df2 = pd.merge(df, mf, on=['conversationWithName'], how='inner')
+
+# augments data
+df3['timeOfDay'] = df3['timestamp'].apply(lambda x: datetime.datetime.fromtimestamp(float(x)).hour) # ?? only up to 12
+df3['dayOfWeek'] = df3['timestamp'].apply(lambda x: datetime.datetime.fromtimestamp(float(x)).weekday())
+
+# sentiment analysis!
+df3 = df2[df2.text.notnull()].dropna()
+df3 = df3[df3.language == 'en']
+df3 = df3[df3.text.map(len) > 3]
+
+sid = SentimentIntensityAnalyzer()
+df3['sentiment'] = df3['text'].apply(lambda x: sid.polarity_scores(x)['compound'])
+
+print ggplot(aes(x='sentiment', fill='senderName'), data=df3[df3.conversationWithName == 'Raluca Halalai']) + geom_density(alpha=0.7)
+print ggplot(aes(x='sentiment', fill='senderName'), data=df3[df3.conversationWithName.str.contains('Ana Trisovic')]) + geom_density(alpha=0.7)
+
+# :D
+print ggplot(aes(x='datetime', y='sentiment', color='senderName'), data=df3[df3.conversationWithName.str.contains('Ralu')][df3.sentiment != 0]) + geom_point(alpha=0.05) + scale_x_date(labels='%b %Y')  + stat_smooth(method='loess')
+print ggplot(df3[df3.text.map(len) < 250], aes(y='dayOfWeek', x='length')) + geom_boxplot()
 
 # compute delta timestamp
 df2['timestamp'] = df2['timestamp'].apply(lambda x: int(x))
@@ -71,6 +92,12 @@ print ggplot(aes(x='timestamp', fill='platform'), data=df) + geom_histogram(alph
 df.col1.str.len().max()
 df.col1.map(lambda x: len(x)).max()
 df.col1.map(len).max()
+
+# filter base on part of string
+df[df['A'].str.contains("hello")]
+
+# keep only rows where text length > 3
+df3[df3.text.map(len) > 3]
 
 # if in lambda
 # lambda x: True if x % 2 == 0 else False
