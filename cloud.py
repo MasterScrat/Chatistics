c1ee08a56e892d6334342f9fc81ae21e8f171955
Misha Behersky <bmwant@gmail.com>
2018-02-08 14:37:02 +0200

    Refactor cloud module

--- cloud.py
+++ cloud.py
@@ -1,103 +1,122 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
+import os
+import re
+import time
+import json
+import argparse
+
 import numpy as np
-from os import path
 import matplotlib.pyplot as plt
 import pandas as pd
 from wordcloud import WordCloud, ImageColorGenerator
 from scipy.misc import imread
-import json
-import re
-import argparse
-import time
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-d','-data', dest='dataPaths', nargs='+', help='chat log data files (pickle files)', required=True)
-parser.add_argument('-sw','-stopwordsPaths', dest='stopwordsPaths', nargs='+', help='stopword files (JSON format)', default=["stopwords/en.json"])
-parser.add_argument('-m', '-maskImage', dest='maskImage', type=str, default=None, help="image to use as mask", required=True)
-parser.add_argument("-filterConversation", dest='filterConversation', type=str, default=None, help="only keep messages sent in a conversation with this sender")
-parser.add_argument("-filterSender", dest='filterSender', type=str, default=None, help="only keep messages sent by this sender")
-parser.add_argument("-removeSender", dest='removeSender', type=str, default=None, help="remove messages sent by this sender")
-parser.add_argument("-n", "-numWords", dest='numWords', type=int, default=10000, help="bin width for histograms")
-parser.add_argument("-density", "-dpi", dest='density', type=int, default=100, help="rendered image DPI")
-args = parser.parse_args()
 
-numWords = args.numWords
-density = args.density
-maskImg = args.maskImage
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--data', dest='data_paths', nargs='+',
+                        help='chat log data files (pickle files)', required=True)
+    parser.add_argument('--sw', '--stopwords-paths', dest='stopwords_paths', nargs='+',
+                        help='stopword files (JSON format)', default=['stopwords/en.json'])
+    parser.add_argument('-m', '--mask-image', dest='mask_image', type=str, default=None,
+                        help='image to use as mask', required=True)
+    parser.add_argument('--filter-conversation', dest='filter_conversation', type=str, default=None,
+                        help='only keep messages sent in a conversation with this sender')
+    parser.add_argument('--filter-sender', dest='filter_sender', type=str, default=None,
+                        help='only keep messages sent by this sender')
+    parser.add_argument('--remove-sender', dest='remove_sender', type=str, default=None,
+                        help='remove messages sent by this sender')
+    parser.add_argument('-n', '--num-words', dest='num_words', type=int, default=10000, help='bin width for histograms')
+    parser.add_argument('--density', '--dpi', dest='density', type=int, default=100, help='rendered image DPI')
+    args = parser.parse_args()
+    return args
 
-dataPaths = args.dataPaths
-stopwordsPaths = args.stopwordsPaths
-filterConversation = args.filterConversation
-filterSender = args.filterSender
-removeSender = args.removeSender
 
-print('Mask image:', maskImg)
-print('Up to', numWords, 'words on the cloud')
-print('Image dpi:', density)
+def main():
+    args = parse_arguments()
+    num_words = args.num_words
+    density = args.density
+    mask_img = args.mask_image
 
-# TODO factor w histo logic
-df = pd.DataFrame()
-for dataPath in dataPaths:
-    print('Loading messages from', dataPath, '...')
-    df = pd.concat([df, pd.read_pickle(dataPath)])
+    data_paths = args.data_paths
+    stopwords_paths = args.stopwords_paths
+    filter_conversation = args.filter_conversation
+    filter_sender = args.filter_sender
+    remove_sender = args.remove_sender
 
-df.columns = ['timestamp', 'conversationId', 'conversationWithName', 'senderName', 'text', 'platform', 'language', 'datetime']
-print('Loaded', len(df), 'messages')
+    print('Mask image:', mask_img)
+    print('Up to', num_words, 'words on the cloud')
+    print('Image dpi:', density)
 
-if filterConversation is not None:
-    print('Keeping only messages in conversations with', filterConversation)
-    df = df[df['conversationWithName'] == filterConversation]
+    # TODO factor w histo logic
+    df = pd.DataFrame()
+    for dataPath in data_paths:
+        print('Loading messages from %s...' % dataPath)
+        df = pd.concat([df, pd.read_pickle(dataPath)])
 
-if filterSender is not None:
-    print('Keeping only messages sent by', filterSender)
-    df = df[df['senderName'] == filterSender]
+    df.columns = ['timestamp', 'conversationId', 'conversationWithName', 'senderName', 'text', 'platform', 'language',
+                  'datetime']
+    print('Loaded', len(df), 'messages')
 
-if removeSender is not None:
-    print('Removing messages sent by', removeSender)
-    df = df[df['senderName'] != removeSender]
+    if filter_conversation is not None:
+        print('Keeping only messages in conversations with', filter_conversation)
+        df = df[df['conversationWithName'] == filter_conversation]
 
-if len(df['text'])==0:
-    print('No messages left! review your filter options')
-    exit(0)
+    if filter_sender is not None:
+        print('Keeping only messages sent by', filter_sender)
+        df = df[df['senderName'] == filter_sender]
 
-print('Final corpus:', len(df['text'])/1000, 'K messages')
+    if remove_sender is not None:
+        print('Removing messages sent by', remove_sender)
+        df = df[df['senderName'] != remove_sender]
 
-stopwords = []
-for stopwordsPath in stopwordsPaths:
-    print('Loading stopwords from', stopwordsPath, '...')
-    stopwords = stopwords + json.load(open(stopwordsPath))
+    if len(df['text']) == 0:
+        print('No messages left! review your filter options')
+        exit(0)
 
-stopwords = set(stopwords)
-print('Stopwords:', len(stopwords), 'words')
+    print('Final corpus:', len(df['text'])/1000, 'K messages')
 
-# pre-compiled regex is faster than going through a list
-stopwords = '|'.join(stopwords)
-regex = re.compile(r'\b('+stopwords+r')\b', re.UNICODE)
+    stopwords = []
+    for stopwordsPath in stopwords_paths:
+        print('Loading stopwords from', stopwordsPath, '...')
+        stopwords = stopwords + json.load(open(stopwordsPath))
 
-print('Cleaning up...')
-text = df['text'] \
-    .replace(to_replace='None', value=np.nan).dropna() \
-    .str.lower() \
-    .apply(lambda w: re.sub(r'^https?:\/\/.*[\r\n]*', '', w)) \
-    .apply(lambda w: regex.sub('', w))
+    stopwords = set(stopwords)
+    print('Stopwords:', len(stopwords), 'words')
 
-text  = ' '.join(text)
+    # pre-compiled regex is faster than going through a list
+    stopwords = '|'.join(stopwords)
+    regex = re.compile(r'\b('+stopwords+r')\b', re.UNICODE)
 
-print('Rendering...')
-d = path.dirname(__file__)
-mask = imread(path.join(d, maskImg))
+    print('Cleaning up...')
+    text = df['text'] \
+        .replace(to_replace='None', value=np.nan).dropna() \
+        .str.lower() \
+        .apply(lambda w: re.sub(r'^https?:\/\/.*[\r\n]*', '', w)) \
+        .apply(lambda w: regex.sub('', w))
 
-# https://amueller.github.io/word_cloud/generated/wordcloud.WordCloud.html#wordcloud.WordCloud
-wc = WordCloud(background_color="white", mask=mask, max_words=numWords, stopwords=None, normalize_plurals=False, collocations=True).generate(text)
+    text = ' '.join(text)
 
-image_colors = ImageColorGenerator(mask)
-wc = wc.recolor(color_func=image_colors)
+    print('Rendering...')
+    directory = os.path.dirname(__file__)
+    mask = imread(os.path.join(directory, mask_img))
 
-plt.imshow(wc)
-plt.axis("off")
+    # https://amueller.github.io/word_cloud/generated/wordcloud.WordCloud.html#wordcloud.WordCloud
+    wc = WordCloud(background_color="white", mask=mask,
+                   max_words=num_words, stopwords=None, normalize_plurals=False, collocations=True).generate(text)
 
-path = 'renders/' + str(int(time.time()*1000)) + '.png'
-print('Saving to', path, '...')
-plt.savefig(path, dpi = density)
+    image_colors = ImageColorGenerator(mask)
+    wc = wc.recolor(color_func=image_colors)
+
+    plt.imshow(wc)
+    plt.axis("off")
+
+    path = 'renders/' + str(int(time.time()*1000)) + '.png'
+    print('Saving to %s...' % path)
+    plt.savefig(path, dpi=density)
+
+
+if __name__ == '__main__':
+    main()
