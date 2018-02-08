bdc0d0346a478d4df7746a1100e83d868923b12b
Misha Behersky <bmwant@gmail.com>
2018-02-08 14:05:55 +0200

    Refactor messenger and hangouts parsers

--- parsers/hangouts.py
+++ parsers/hangouts.py
@@ -4,114 +4,119 @@
 from __future__ import print_function
 import json
 import pandas as pd
-import numpy as np
 import datetime
 import argparse
 from random import randint
 from langdetect import *
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-ownName", dest='ownName', type=str, help="name of the owner of the chat logs, written as in the logs", required=True)
-parser.add_argument('-f','-filePath', dest='filePath', help='Facebook chat log file (HTML file)', default='raw/Hangouts.json')
-parser.add_argument("-max", "-maxExportedMessages", dest='maxExportedMessages', type=int, default=1000000, help="maximum number of messages to export")
-args = parser.parse_args()
 
-maxExportedMessages = args.maxExportedMessages
-ownName = args.ownName
-filePath = args.filePath
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--own-name', dest='own_name', type=str,
+                        help='name of the owner of the chat logs, written as in the logs', required=True)
+    parser.add_argument('-f','--file-path', dest='file_path', help='Hangouts chat log file',
+                        default='raw/Hangouts.json')
+    parser.add_argument('--max', '--max-exported-messages', dest='max_exported_messages', type=int, default=1000000,
+                        help='maximum number of messages to export')
+    args = parser.parse_args()
+    return args
 
-print('Parsing JSON file...')
-archive = json.load(open(filePath))
 
-names = {}
-def idToName(id):
-	if id in names:
-		return names[id]
-	else:
-		return None
+def export_dataframe(df):
+    print('Saving to pickle file...')
+    df.to_pickle('data/hangouts.pkl')
 
-def saveNameForId(name, id):
-	if not id in names:
-		names[id] = name
-	elif names[id] != name:
-		print('Assuming', name, 'is', names[id])
 
-data = []
-conversationId = ""
-conversationWithId = ""
-conversationWithName = ""
+def main():
+    args = parse_arguments()
+    own_name = args.own_name
 
-print('Extracting messages...')
-for state in archive["conversation_state"]:
+    print('Parsing JSON file...')
+    archive = json.loads(args.file_path)
 
-	if "conversation_id" in state:
-		conversationId = state["conversation_id"]["id"]
+    names = {}
 
-	if "conversation" in state["conversation_state"]:
-		for participant in state["conversation_state"]["conversation"]["participant_data"]:
-			if "fallback_name" in participant:
-				saveNameForId(participant["fallback_name"], participant["id"]["gaia_id"])
+    def idToName(id):
+        if id in names:
+            return names[id]
+        else:
+            return None
 
-	for event in state["conversation_state"]["event"]:
-		timestamp = int(event["timestamp"])
+    def saveNameForId(name, id):
+        if not id in names:
+            names[id] = name
+        elif names[id] != name:
+            print('Assuming', name, 'is', names[id])
 
-		if "conversation_id" in event:
-			conversationId = event["conversation_id"]["id"]
+    data = []
+    conversationWithId = ''
+    conversationWithName = ''
 
-		if "chat_message" in event and "segment" in event["chat_message"]["message_content"]:
-			content = event["chat_message"]["message_content"]
-			text = content["segment"][0]["text"]
-			conversationId = event["conversation_id"]["id"]
-			senderId = event["sender_id"]["chat_id"]
+    print('Extracting messages...')
+    for state in archive["conversation_state"]:
+        if "conversation" in state["conversation_state"]:
+            for participant in state["conversation_state"]["conversation"]["participant_data"]:
+                if "fallback_name" in participant:
+                    saveNameForId(participant["fallback_name"], participant["id"]["gaia_id"])
 
-			participants = state["conversation_state"]["conversation"]["current_participant"]
+        for event in state["conversation_state"]["event"]:
+            timestamp = int(event["timestamp"])
 
-			if len(participants) == 2:
-				for participant in participants:
-					if idToName(participant["gaia_id"]) != ownName:
-						conversationWithId = participant["gaia_id"]
+            if "chat_message" in event and "segment" in event["chat_message"]["message_content"]:
+                content = event["chat_message"]["message_content"]
+                text = content["segment"][0]["text"]
+                conversationId = event["conversation_id"]["id"]
+                senderId = event["sender_id"]["chat_id"]
 
-				if idToName(senderId)!=None or idToName(conversationWithId)!=None:
-					if idToName(senderId) != ownName and senderId != conversationWithId:
-						#print idToName(senderId), 'in conversation with', idToName(conversationWithId), '!'
-						print('Parsing error, is your ownId correct?')
-						exit(0)
+                participants = state["conversation_state"]["conversation"]["current_participant"]
 
-					# saves the message
-					timestamp = timestamp/1000000
-					data += [[timestamp, conversationId, idToName(conversationWithId), idToName(senderId), text]]
+                if len(participants) == 2:
+                    for participant in participants:
+                        if idToName(participant["gaia_id"]) != own_name:
+                            conversationWithId = participant["gaia_id"]
 
-				else:
-					# unknown sender
-					print("No senderName for either senderId", senderId, conversationWithId)
+                    if idToName(senderId) is not None or idToName(conversationWithId) is not None:
+                        if idToName(senderId) != own_name and senderId != conversationWithId:
+                            # print idToName(senderId), 'in conversation with', idToName(conversationWithId), '!'
+                            print('Parsing error, is your ownId correct?')
+                            exit(0)
 
-				if len(data) >= maxExportedMessages:
-					break
+                        # saves the message
+                        timestamp = timestamp/1000000
+                        data += [[timestamp, conversationId, idToName(conversationWithId), idToName(senderId), text]]
 
-print(len(data), 'messages parsed.')
+                    else:
+                        # unknown sender
+                        print("No senderName for either senderId", senderId, conversationWithId)
 
-print('Converting to DataFrame...')
-df = pd.DataFrame(index=np.arange(0, len(data)))
-df = pd.DataFrame(data)
-df.columns = ['timestamp', 'conversationId', 'conversationWithName', 'senderName', 'text']
-df['platform'] = 'hangouts'
+                    if len(data) >= args.max_exported_messages:
+                        break
 
-print('Detecting languages...')
-df['language'] = 'unknown'
-for name, group in df.groupby(df.conversationWithName):
-	sample = ''
-	df2 = df[df.conversationWithName == name].dropna()
+    print(len(data), 'messages parsed.')
 
-	if len(df2)>10:
-		for x in range(0, min(len(df2), 100)):
-			sample = sample + df2.iloc[randint(0, len(df2)-1)]['text']
+    print('Converting to DataFrame...')
+    df = pd.DataFrame(data)
+    df.columns = ['timestamp', 'conversationId', 'conversationWithName', 'senderName', 'text']
+    df['platform'] = 'hangouts'
 
-		print('\t', name, detect(sample))
-		df.loc[df.conversationWithName == name, 'language'] = detect(sample)
+    print('Detecting languages...')
+    df['language'] = 'unknown'
+    for name, group in df.groupby(df.conversationWithName):
+        sample = ''
+        df2 = df[df.conversationWithName == name].dropna()
 
-print('Computing dates...')
-df['datetime'] = df['timestamp'].apply(lambda x: datetime.datetime.fromtimestamp(float(x)).toordinal())
-print(df.head())
+        if len(df2)>10:
+            for x in range(0, min(len(df2), 100)):
+                sample = sample + df2.iloc[randint(0, len(df2)-1)]['text']
 
-print('Saving to pickle file...')
-df.to_pickle('data/hangouts.pkl')
+            print('\t', name, detect(sample))
+            df.loc[df.conversationWithName == name, 'language'] = detect(sample)
+
+    print('Computing dates...')
+    df['datetime'] = df['timestamp'].apply(lambda x: datetime.datetime.fromtimestamp(float(x)).toordinal())
+    print(df.head())
+    export_dataframe(df)
+
+
+if __name__ == '__main__':
+    main()
