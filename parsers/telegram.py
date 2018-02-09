5c11b92957274d31c958495b83c6d34d77612eef
flaurent <florian.laurent@gmail.com>
2018-02-09 17:23:02 +0100

    Ignore messages from delete accounts

--- parsers/telegram.py
+++ parsers/telegram.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+import sys
+
+if sys.version_info < (3, 0):
+    sys.stdout.write("Sorry, requires Python 3.\n")
+    sys.exit(1)
+
 import pandas as pd
 from telethon import TelegramClient
 from telethon.tl.types import PeerUser, PeerChannel, PeerChat
@@ -34,16 +40,21 @@ def sign_in(client):
 
 
 def process_dialog_with_user(client, item):
+    result = []
     conversation_with_name = item.name
+
+    # deleted account
+    if conversation_with_name == '': return result
+
     dialog = item.dialog
     user_id = dialog.peer.user_id
     limit = config.TELEGRAM_USER_DIALOG_MESSAGES_LIMIT
-    result = []
     messages = client.get_message_history(user_id, limit=limit)
     for message in messages:
         timestamp = message.date.timestamp()
         ordinal_date = message.date.toordinal()
         text = message.message
+        print(timestamp, user_id, conversation_with_name, '', text, 'unknown', '', ordinal_date)
         result.append([timestamp, user_id, conversation_with_name, '', text, 'unknown', '', ordinal_date])
     return result
 
