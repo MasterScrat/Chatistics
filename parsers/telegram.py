571a6d3a211ecc1c8b7f32f5a37307567348f30d
Martin Müller <martin.mathias.mueller@gmail.com>
2019-11-30 15:17:33 +0100

    re-write of telegram parser using new API

--- parsers/telegram.py
+++ parsers/telegram.py
@@ -1,45 +1,25 @@
 #!/usr/bin/env python3
-import sys
-
-if sys.version_info < (3, 0):
-    sys.stdout.write("Sorry, requires Python 3.\n")
-    sys.exit(1)
-
 import pandas as pd
 from telethon import TelegramClient
 from telethon.tl.types import PeerUser, PeerChannel, PeerChat
-
 from parsers import log
 from parsers import utils, config
 
 
-def list_dialogs(client):
-    dialogs = client.get_dialogs()
+async def list_dialogs(client):
     result = []
-    for item in dialogs:
+    async for item in client.iter_dialogs():
         dialog = item.dialog
         if isinstance(dialog.peer, PeerUser):
-            result.extend(process_dialog_with_user(client, item))
+            _r = await process_dialog_with_user(client, item)
+            result.extend(_r)
         elif isinstance(dialog.peer, (PeerChannel, PeerChat)):
             log.debug('Dialogs in chats/channels are not supported yet')
         else:
             log.warning('Unknown dialog type %s', dialog)
-
     return result
 
-
-def sign_in(client):
-    print('Logging into account {}...'.format(config.TELEGRAM_PHONE))
-    if not client.is_user_authorized():
-        client.send_code_request(config.TELEGRAM_PHONE)
-        code = input('Enter code received: ')
-        me = client.sign_in(code=code)
-    else:
-        me = client.get_me()
-    return me
-
-
-def process_dialog_with_user(client, item):
+async def process_dialog_with_user(client, item):
     result = []
     conversation_with_name = item.name
 
@@ -49,34 +29,31 @@ def process_dialog_with_user(client, item):
     dialog = item.dialog
     user_id = dialog.peer.user_id
     limit = config.TELEGRAM_USER_DIALOG_MESSAGES_LIMIT
-    messages = client.get_message_history(user_id, limit=limit)
-    for message in messages:
+    async for message in client.iter_messages(user_id, limit=limit):
         timestamp = message.date.timestamp()
         ordinal_date = message.date.toordinal()
         text = message.message
         result.append([timestamp, user_id, conversation_with_name, '', text, 'unknown', '', ordinal_date])
     return result
 
-
-def main():
-    client = TelegramClient('session_name', config.TELEGRAM_API_ID, config.TELEGRAM_API_HASH)
-    client.connect()
-    me = sign_in(client)
-    data = list_dialogs(client)
+async def _main_loop(client):
+    me = await client.get_me()
+    data = await list_dialogs(client)
     log.info('Converting to DataFrame...')
     df = pd.DataFrame(data)
     df.columns = config.ALL_COLUMNS
-    # import pdb; pdb.set_trace()
     df['platform'] = 'telegram'
     own_name = '{} {}'.format(me.first_name, me.last_name).strip()
     df['senderName'] = own_name
-
     log.info('Detecting languages...')
     df['language'] = 'unknown'
-
     utils.export_dataframe(df, 'telegram.pkl')
     log.info('Done.')
 
+
+def main():
+    with TelegramClient('session_name', config.TELEGRAM_API_ID, config.TELEGRAM_API_HASH) as client:
+        client.loop.run_until_complete(_main_loop(client))
 
 if __name__ == '__main__':
     main()
